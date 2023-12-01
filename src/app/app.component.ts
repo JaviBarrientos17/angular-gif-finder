7306f1c7ae253a25e6bf9cc5e7cde048b2074e1e
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-12-01 11:00:24 +0100

    Divided app.component code in components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { GiphyService } from './giphy.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'angular-gif-finder';
-  gifs: any[] = [];
-
-  constructor(private giphyService: GiphyService) {}
-
-  ngOnInit(): void {
-    this.search('panda');
-  }
-
-  search(query: string): void {
-    this.giphyService.searchGifs(query).subscribe((response) => {
-      this.gifs = response.data;
-    });
-  }
 }
---
+++ src/components/gifGrid/gifGrid.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { GiphyService } from 'src/app/giphy.service';
+
+@Component({
+  selector: 'gifGrid-component',
+  templateUrl: './gifGrid.component.html',
+  styleUrls: ['./gifGrid.component.css'],
+})
+export class GifGridComponent implements OnInit {
+  gifs: any[] = [];
+
+  constructor(private giphyService: GiphyService) {}
+
+  ngOnInit(): void {
+    this.search('panda');
+  }
+
+  search(query: string): void {
+    this.giphyService.searchGifs(query).subscribe((response) => {
+      this.gifs = response.data;
+    });
+  }
+}
