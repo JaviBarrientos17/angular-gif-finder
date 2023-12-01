89e6d14f867bed4bef41656dc1d7ca9d867bbfcc
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-12-01 12:36:31 +0100

    Reworked pkgs distribution & added footer comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,19 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from 'src/components/navbar/navbar.component';
-import { GifGridComponent } from 'src/components/gifGrid/gifGrid.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GifGridComponent } from './components/gifGrid/gifGrid.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     GifGridComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/gifGrid/gifGrid.component.ts
+++ src/app/components/gifGrid/gifGrid.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { GiphyService } from 'src/app/giphy.service';
+import { GiphyService } from 'src/app/services/giphy.service';
 
 @Component({
   selector: 'gifGrid-component',
