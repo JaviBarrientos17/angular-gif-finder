799731c67cd667ee793550ee8d2fec69a776d685
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-12-12 16:31:38 +0100

    Working on scroll gif auto load

--- src/app/components/gifGrid/gifGrid.component.ts
+++ src/app/components/gifGrid/gifGrid.component.ts
@@ -1,6 +1,6 @@
 // gifGrid.component.ts
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GiphyService } from 'src/app/services/giphy.service';
 
 @Component({
@@ -8,23 +8,33 @@ import { GiphyService } from 'src/app/services/giphy.service';
   templateUrl: './gifGrid.component.html',
   styleUrls: ['./gifGrid.component.css'],
 })
-export class GifGridComponent {
+export class GifGridComponent implements OnInit {
   gifs: any[] = [];
   searchQuery: string = '';
   isSearching: boolean = false;
   noResultsMessage: string | null = null; // Nuevo campo para el mensaje
+  loadingMore: boolean = false;
+  offset: number = 0; // Nuevo campo para manejar la paginación
 
   constructor(private giphyService: GiphyService) {}
 
+  ngOnInit(): void {
+    // Cargar trending GIFs al inicializar el componente
+    this.loadTrendingGifs();
+  }
+
   search(): void {
     if (this.searchQuery.trim() === '') {
-      return; // Evitar búsqueda vacía
+      // Si la búsqueda está vacía, cargar trending GIFs
+      this.loadTrendingGifs();
+      return;
     }
 
     this.isSearching = true;
     this.noResultsMessage = null; // Reiniciar el mensaje
+    this.offset = 0; // Reiniciar el offset para la búsqueda
 
-    this.giphyService.searchGifs(this.searchQuery).subscribe(
+    this.giphyService.searchGifs(this.searchQuery, this.offset).subscribe(
       (response) => {
         this.gifs = response.data;
 
@@ -38,6 +48,70 @@ export class GifGridComponent {
       (error) => {
         console.error('Error fetching gifs', error);
         this.isSearching = false;
+      }
+    );
+  }
+
+  private loadTrendingGifs(): void {
+    this.isSearching = true;
+    this.noResultsMessage = null;
+
+    this.giphyService.getTrendingGifs().subscribe(
+      (response) => {
+        this.gifs = response.data;
+
+        // Verificar si no hay resultados
+        if (this.gifs.length === 0) {
+          this.noResultsMessage = 'No se encontraron trending GIFs.';
+        }
+
+        this.isSearching = false;
+      },
+      (error) => {
+        console.error('Error fetching trending gifs', error);
+        this.isSearching = false;
+      }
+    );
+  }
+
+  onScroll(): void {
+    const element = document.documentElement;
+    const scrollHeight = element.scrollHeight;
+    const scrollTop = element.scrollTop;
+    const clientHeight = element.clientHeight;
+
+    if (
+      scrollTop + clientHeight >= scrollHeight - 200 &&
+      !this.isSearching &&
+      !this.loadingMore
+    ) {
+      this.loadMoreGifs();
+    }
+  }
+
+  loadMoreGifs(): void {
+    this.loadingMore = true;
+
+    // Lógica para cargar más GIFs aquí
+    this.offset += 25; // Ajusta el valor según la paginación de la API
+
+    this.giphyService.searchGifs(this.searchQuery, this.offset).subscribe(
+      (response) => {
+        const newGifs = response.data;
+
+        // Verificar si hay nuevos resultados
+        if (newGifs.length > 0) {
+          this.gifs = [...this.gifs, ...newGifs];
+        } else {
+          // Si no hay más resultados, puedes desactivar la funcionalidad de carga
+          // o mostrar un mensaje indicando que no hay más resultados.
+        }
+
+        this.loadingMore = false;
+      },
+      (error) => {
+        console.error('Error fetching more gifs', error);
+        this.loadingMore = false;
       }
     );
   }
--- src/app/services/giphy.service.ts
+++ src/app/services/giphy.service.ts
@@ -11,9 +11,14 @@ export class GiphyService {
 
   constructor(private http: HttpClient) {}
 
-  searchGifs(query: string): Observable<any> {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${this.apiKey}`;
+  searchGifs(query: string, offset: number = 0): Observable<any> {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${this.apiKey}&offset=${offset}`;
     console.log(query);
+    return this.http.get(url);
+  }
+
+  getTrendingGifs(): Observable<any> {
+    const url = `https://api.giphy.com/v1/gifs/trending?api_key=${this.apiKey}`;
     return this.http.get(url);
   }
 }
